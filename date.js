d7c41d2d1f23281b39be532a6f8cae41b10d782c
Salaum_JohnCarlo <johncarlosalaum@gmail.com>
2024-02-26 12:28:03 +0800

    Add 2 Features

---
+++ date.js
@@ -0,0 +1,13 @@
+function updateDateTime() {
+    var currentDate = new Date();
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    var formattedDate = currentDate.toLocaleDateString('en-US', options);
+
+    document.getElementById('datetime').textContent = formattedDate;
+}
+
+
+setInterval(updateDateTime, 1000);
+
+
+updateDateTime();
